3f0cd9f4cd1cfc69ca87bb484c11b5fc7bfb905f
saintajax <108813003+saintajax@users.noreply.github.com>
2022-08-28 14:39:58 +0000

    Deploying to gh-pages from @ saintajax/team-project@67ecb26cfc0ee163bb60340d99c30979a3161810 🚀

---
+++ index.e3e63932.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{slidesPerView:1,spaceBetween:30,keyboard:{enabled:!0},pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".swiper1",{spaceBetween:30,centeredSlides:!0,autoplay:{delay:5e3},loop:!0,effect:"fade"});
+//# sourceMappingURL=index.e3e63932.js.map
---
+++ index.eb3961b3.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{slidesPerView:1,spaceBetween:30,keyboard:{enabled:!0},pagination:{el:".swiper-pagination",clickable:!0}}),new Swiper(".swiper1",{spaceBetween:30,centeredSlides:!0,autoplay:{delay:5e3},loop:!0,effect:"fade"});
+//# sourceMappingURL=index.eb3961b3.js.map
