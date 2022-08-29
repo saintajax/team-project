6dbe0738ab996e448d2b6acf901d219ae3d7770c
saintajax <saintajax@gmail.com>
2022-08-29 03:46:11 +0300

    flipped

---
+++ src/js/products.js
@@ -0,0 +1,14 @@
+var card = document.querySelector('.card');
+card.addEventListener('click', function () {
+  card.classList.toggle('is-flipped');
+});
+
+var card2 = document.querySelector('.card2');
+card.addEventListener('click', function () {
+  card.classList.toggle('is-flipped2');
+});
+
+var card3 = document.querySelector('.card3');
+card.addEventListener('click', function () {
+  card.classList.toggle('is-flipped3');
+});
