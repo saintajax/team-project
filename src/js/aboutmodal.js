c978e549738b6cafaa34078fc70f780d9c59c146
saintajax <saintajax@gmail.com>
2022-08-30 00:45:33 +0300

    modals fix

---
+++ src/js/aboutmodal.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-about').onclick = function () {
+  document.getElementById('modal-about').classList.toggle('active');
+  document.getElementById('overlay').classList.toggle('active');
+};
