988e1e48a3c36d2101596727727c196cc86151cf
saintajax <108813003+saintajax@users.noreply.github.com>
2022-08-29 09:12:41 +0000

    Deploying to gh-pages from @ saintajax/team-project@2cac39e6c94ea7af7b3c2278611afdf0ac21d388 🚀

---
+++ index.09916d8d.js
@@ -0,0 +1,2 @@
+document.getElementById("btn-open").onclick=function(){document.getElementById("btn-buy").classList.toggle("is-left"),document.getElementById("nav-site").classList.toggle("is-left")};
+//# sourceMappingURL=index.09916d8d.js.map
---
+++ index.b5010898.js
@@ -0,0 +1,2 @@
+document.getElementById("btn-open").onclick=function(){document.getElementById("btn-buy").classList.toggle("is-left"),document.getElementById("nav-site").classList.toggle("is-left")};
+//# sourceMappingURL=index.b5010898.js.map
