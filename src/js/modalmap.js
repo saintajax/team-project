c77f5308217579679cd78992a2e7090fd92a5a8d
saintajax <saintajax@gmail.com>
2022-08-29 23:24:28 +0300

    map js

---
+++ src/js/modalmap.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-map').onclick = function () {
+  document.getElementById('map').classList.toggle('active');
+  document.getElementById('overlay').classList.toggle('active');
+};
