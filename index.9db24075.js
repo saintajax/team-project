d4109cc28058b1001023fefc892d59fdb2aa91e6
saintajax <108813003+saintajax@users.noreply.github.com>
2022-08-30 10:59:26 +0000

    Deploying to gh-pages from @ saintajax/team-project@d4c52cae11e892fec3216402c9ddef866ffeba11 🚀

---
+++ index.9db24075.js
@@ -0,0 +1,2 @@
+function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("button").style.display="block":document.getElementById("button").style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.9db24075.js.map
