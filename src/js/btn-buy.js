d7430ea9c513d6446d3c89add3ee684130a201dc
saintajax <saintajax@gmail.com>
2022-08-30 09:21:46 +0300

    fix

--- src/js/btn-buy.js
+++ src/js/btn-buy.js
@@ -1,6 +1,6 @@
 // (() => {
 //     const menuBtnBuy = document.querySelector('.button--header');
-  
+
 //     menuBtnBuy.addEventListener("click", () => {
 
 //       menuBtnBuy.classList.toggle("is-left");
@@ -8,7 +8,6 @@
 //     });
 //   })();
 
-  document.getElementById('btn-open').onclick = function() {
-    document.getElementById('btn-buy').classList.toggle('is-left');
-    document.getElementById('nav-site').classList.toggle('is-left');
-  }
+document.getElementById('btn-open').onclick = function () {
+  document.getElementById('btn-buy').classList.toggle('is-left');
+};
