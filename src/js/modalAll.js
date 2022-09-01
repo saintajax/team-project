86a7530e8f19c04ad163797c4dc5b67affb2eadb
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-09-01 18:49:31 +0300

    fixes area btn cls modal

--- src/js/modalAll.js
+++ src/js/modalAll.js
@@ -1,30 +1,3 @@
-!(function (e) {
-  'function' != typeof e.matches &&
-    (e.matches =
-      e.msMatchesSelector ||
-      e.mozMatchesSelector ||
-      e.webkitMatchesSelector ||
-      function (e) {
-        for (
-          var t = this,
-            o = (t.document || t.ownerDocument).querySelectorAll(e),
-            n = 0;
-          o[n] && o[n] !== t;
-
-        )
-          ++n;
-        return Boolean(o[n]);
-      }),
-    'function' != typeof e.closest &&
-      (e.closest = function (e) {
-        for (var t = this; t && 1 === t.nodeType; ) {
-          if (t.matches(e)) return t;
-          t = t.parentNode;
-        }
-        return null;
-      });
-})(window.Element.prototype);
-
 document.addEventListener('DOMContentLoaded', function () {
   const modalButtons = document.querySelectorAll('.js-open-modal'),
     overlay = document.querySelector('.js-overlay-modal'),
@@ -34,27 +7,24 @@ document.addEventListener('DOMContentLoaded', function () {
   modalButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
       e.preventDefault();
-
       const modalId = this.getAttribute('data-modal'),
         modalElem = document.querySelector(
           '.modal[data-modal="' + modalId + '"]'
         );
-
       modalElem.classList.add('active');
       overlay.classList.add('active');
       bodyscroll.classList.add('lock');
     });
   });
+
   closeButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
-      const parentModal = this.closest('.modal');
 
-      parentModal.classList.remove('active');
+      document.querySelector('.modal.active').classList.remove('active');
       overlay.classList.remove('active');
       document.querySelector('.data-body').classList.remove('lock');
     });
   });
-
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
