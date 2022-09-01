60f622b13ef1779ee822e321d2bcbc8640a4092d
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-09-01 11:23:04 +0300

    addes btn up smooth

--- src/js/to-top.js
+++ src/js/to-top.js
@@ -31,16 +31,25 @@
 
 // });
 
+const mediaDesktop = window.matchMedia("(min-width: 1200px)");
+const mediaTablet = window.matchMedia("(max-width: 1199px)");
+
+if(mediaDesktop.matches){
+
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("button").style.display = "block";
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("button").style.display = "flex";
+        document.getElementById("button").style.opacity = "1";
     } else {
-        document.getElementById("button").style.display = "none";
+        document.getElementById("button").style.opacity = "0";
     }
 }
-function topFunction() {
+
+function topFunction(){
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0;
 }
+} else(mediaTablet.matches) 
+    document.getElementById("button").style.display = "none";
