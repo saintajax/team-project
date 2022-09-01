f2a11d72630554b28e9db5b75b973bce9dd5aa54
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-09-01 21:55:39 +0300

    modalAll data-attribute

--- src/js/modalAll.js
+++ src/js/modalAll.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const modalButtons = document.querySelectorAll('.js-open-modal'),
-    overlay = document.querySelector('.js-overlay-modal'),
-    closeButtons = document.querySelectorAll('.js-modal-close'),
-    bodyscroll = document.querySelector('.data-body');
+  const modalButtons = document.querySelectorAll('[js-open-modal]'),
+    overlay = document.querySelector('[js-overlay-modal]'),
+    closeButtons = document.querySelectorAll('[js-modal-close]'),
+    bodyscroll = document.querySelector('[data-body]');
 
   modalButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
@@ -22,12 +22,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
       document.querySelector('.modal.active').classList.remove('active');
       overlay.classList.remove('active');
-      document.querySelector('.data-body').classList.remove('lock');
+      document.querySelector('[data-body]').classList.remove('lock');
     });
   });
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
-    document.querySelector('.data-body').classList.remove('lock');
+    document.querySelector('[data-body]').classList.remove('lock');
   });
 });
