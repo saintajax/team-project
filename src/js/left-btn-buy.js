b9bea003eff6597b0f4734e3c1e082463436c45f
ArturMand <108627874+ArturMand@users.noreply.github.com>
2022-09-01 16:24:42 +0300

    adds unscroll when modal/menu is open

---
+++ src/js/left-btn-buy.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-open').onclick = function () {
+  document.getElementById('btn-buy').classList.toggle('is-left');
+};
--- src/js/to-top.js
+++ src/js/to-top.js
@@ -1,55 +1,25 @@
-// var btn = $('#button');
+const mediaDesktop = window.matchMedia('(min-width: 1200px)');
+const mediaTablet = window.matchMedia('(max-width: 1199px)');
 
-// $(window).scroll(function() {
-//   if ($(window).scrollTop() > 300) {
-//     btn.addClass('show');
-//   } else {
-//     btn.removeClass('show');
-//   }
-// });
-
-// btn.on('click', function(e) {
-//   e.preventDefault();
-//   $('html, body').animate({scrollTop:0}, '300');
-// });
-
-
-// $(window).scroll(function() {
-//   var height = $(window).scrollTop();
-//   if (height > 100) {
-//       $('#button').fadeIn();
-//   } else {
-//       $('#button').fadeOut();
-//   }
-// });
-// $(document).ready(function() {
-//   $("#button").click(function(event) {
-//       event.preventDefault();
-//       $("html, body").animate({ scrollTop: 0 }, "slow");
-//       return false;
-//   });
-
-// });
-
-const mediaDesktop = window.matchMedia("(min-width: 1200px)");
-const mediaTablet = window.matchMedia("(max-width: 1199px)");
-
-if(mediaDesktop.matches){
-
-
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-        document.getElementById("button").style.display = "flex";
-        document.getElementById("button").style.opacity = "1";
+if (mediaDesktop.matches) {
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 500 ||
+      document.documentElement.scrollTop > 500
+    ) {
+      document.getElementById('button').style.display = 'flex';
+      document.getElementById('button').style.opacity = '1';
     } else {
-        document.getElementById("button").style.opacity = "0";
+      document.getElementById('button').style.opacity = '0';
     }
-}
+  }
 
-function topFunction(){
-    document.body.scrollTop = 0; 
+  function topFunction() {
+    document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
-} else(mediaTablet.matches) 
-    document.getElementById("button").style.display = "none";
+  }
+} else mediaTablet.matches;
+document.getElementById('button').style.display = 'none';
